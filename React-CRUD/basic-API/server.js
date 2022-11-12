440663d18d0078e6f9f51b50047f5762cf9a039d
marinabribeiro <49520731+marinabribeiro@users.noreply.github.com>
2022-11-12 01:27:45 -0300

    Delete React-CRUD directory

--- React-CRUD/basic-API/server.js
+++
@@ -1,6 +0,0 @@
-require('./src/infrastructure/database');
-const app = require('./src/infrastructure/rest_server');
-
-app.listen(4000, () => {
-    console.log('REST server running on port 4000...');
-});
--- React-CRUD/basic-front/src/services/config.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const client = axios.create({
-  baseURL: 'http://localhost:4000',
-  timeout: 9000,
-});
-
-export default client;
